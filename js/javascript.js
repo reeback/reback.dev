ec5024df54684f548acd89a84122b9470f613a3a
Reback <gregreback@gmail.com>
2026-02-10 11:05:06 -0500

    www

--- js/javascript.js
+++ js/javascript.js
@@ -1,2 +1,4 @@
+console.log("Witaj świecie!");
+
 document.getElementById("demo1").innerHTML = "Udar mózgu to poważne schorzenie wymagające natychmiastowej pomocy medycznej.";
 document.getElementById("demo2").innerHTML = "Dostałem udaru mózgu i nie mogę mówić";
