523c090fa7006d54f96802d2a3a2d0a24f589e71
Reback <gregreback@gmail.com>
2026-02-11 11:24:03 -0500

    www

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,9 @@
-console.log("Witaj świecie!");
+console.log("Witaj świecie!");  // To jest mój pierwszy skrypt JavaScript.
 
-document.getElementById("demo1").innerHTML = "Udar mózgu to poważne schorzenie wymagające natychmiastowej pomocy medycznej.";
-document.getElementById("demo2").innerHTML = "Dostałem udaru mózgu i nie mogę mówić";
+console.log("To jest mój pierwszy skrypt JavaScript."); // Ten skrypt wyświetla tekst w konsoli przeglądarki.
+console.log("JavaScript jest językiem programowania używanym do tworzenia interaktywnych stron internetowych."); // Ten skrypt wyświetla informacje o JavaScript w konsoli przeglądarki.
+
+document.getElementById("demo1").innerHTML = "Udar mózgu to poważne schorzenie wymagające natychmiastowej pomocy medycznej."; // Ten skrypt zmienia zawartość elementu HTML o id "demo1" na podany tekst.
+document.getElementById("demo2").innerHTML = "Dostałem udaru mózgu i nie mogę mówić"; // Ten skrypt zmienia zawartość elementu HTML o id "demo2" na podany tekst.js
+
+let firstName = "Jan"; //
